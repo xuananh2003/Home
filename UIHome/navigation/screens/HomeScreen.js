7c7304e0a315057c661d33de0841cfd4ca746770
Gradi0us <hieunguyen20032302@gmail.com>
2023-05-23 15:38:09 +0700

    Update HomeScreen.js

--- UIHome/navigation/screens/HomeScreen.js
+++ UIHome/navigation/screens/HomeScreen.js
@@ -29,8 +29,9 @@ export default function HomeScreen({ navigation }) {
           }}
         >
         
-        <FontAwesomeIcon icon="shopping-cart" size={20} />
-        
+      
+        <Image source={{uri:'https://lh3.googleusercontent.com/C66JC4hTzdI0CvH364WgkTyGEZrHl2xqDfMUG-1pykcutRG2R_ghB54h5OqjTuDa4apkeY7iuyfRNgc6Jrjvr2k-bBOpeQItdoFyLcVqL03u_Sylf3y0WSD1qOulRc69Oaq55GhaMRMZ4b95k3gxpvcU3UTqJcZz9MhX2LIl9rugfVczsVwk8eHuZlXBy03_lMpytbPEfpp_SZ6CNMYMdbjBbAI3-jA8p4SociW1uDdu-t-714u-vrlbjfh5bArgrU27sC7lY-OzINYURDnFZcJKTON2m4bUKwMQSrbPM93jY_MCpEbeH-itPIRtN1XaLX0pwiCmADyXFjSWtI0hveyGNYgILZpTJmlLNmkUPM0Z4bf0ayLG7Sh68mRIiQK8yoo1CCp24rSG8eyi-g0rZRR3O4GOV2RS8gxQ7Nn6ptcrAqU-0KfI8VdUk4Xie7508ZgoKkB-jLqgGlWwWuy19MzYNGtY9lMVGnRCq_n9dhN5uYu6gWsitBpVYTc392cEPzSItyaPWYmPwUX_5SHH3yhrKSHmwc4NOVM4xA5Ow-XB1SZ8eIN6z4Qk36CFK76ocU_BukwpkDWu5kkzuwCYCWDJNAxltO3PPayfh4FiBMEEEvb9wYMwP-RRrGoM6lSqCVOo9RbJhLaBoi_woEUOsUu1F7i8ep4CrcGZDKV-R6YPkwjKSd5ADTdXaIBgvEfY5rB1xFINHfVNzJnCn4LbW3q11Y813AzCb4F4KrQ4pwpYOPdOU1awA9hhJWx3O8HTrRHt530GYKhwADzgMcqUOWS2mjiAkHuKSb-xaIKIKwgMk_iZ-yY4SC49Me7tqjIfo3gaw585h40Qoi2wDaqks6le9xbyB-5RrrVIhig9J_UQpJ9vZLcH5daYjCyUMppC-p4OTuCnN0JCzfbwKnn41L_ZbW9HCVQ46clkgw5JrmpzbKrOKNxD_fOGgUEvM46z9uBark1zMMQcbFBEEGjAN7zkpp9OKnGTGkeinjRhEKB3EyBs-dSaRCg'}} style={{  width: 20,
+        height:20,}}/>
         </View>
       </View>
       <View style={{ marginLeft: 20, marginTop: 30 }}>
